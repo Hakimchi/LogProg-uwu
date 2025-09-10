3a8a73468efb0d31805567542649048d0caec957
Hakeem Espejel <espejel.torres.hakeem@gmail.com>
2025-09-10 02:45:13 -0600

    Initial Commit

---
+++ PracticaC1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PracticaC1
+//
+//  Created by Hakeem Espejel on 10/09/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
