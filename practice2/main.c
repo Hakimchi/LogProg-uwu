e9bad713a0389c6fc8b22852e13ccfabc5bbaea0
Hakeem Espejel <espejel.torres.hakeem@gmail.com>
2025-09-24 21:53:17 -0600

    Practica 2 de laboratorio

--- practice2/main.c
+++ practice2/main.c
@@ -1,14 +1,18 @@
 //
-//  main.c
-//  practice2
+// main.c
+// práctica modular usando funciones
 //
-//  Created by Hakeem Espejel on 11/09/25.
+// Created by Hakeem Zahid Espejel Torres on 11/09/25.
 //
 
 #include <stdio.h>
+#include "ejercicios.h"  // Incluye las funciones de los ejercicios
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main(void) {
+    // Llamamos a cada ejercicio
+    ejercicio1();
+    ejercicio2();
+    ejercicio3();
+
+    return 0; // Fin del programa
 }
